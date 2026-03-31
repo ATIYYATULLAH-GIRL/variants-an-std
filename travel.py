97e770bab1c63289654585267ef74c9ed970b4ac
ATIYYATULLAH-GIRL <mabas2014.am@gmail.com>
2026-03-31 09:54:04 -0700

    Add files via upload

---
+++ travel.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+data=pd.read_csv("Weather Dataset.csv")
+print(data.head())
+
+print(data.info())
+
+print(data.isnull().sum())
+
+mean_temperature=np.mean(data["Temperature(C)"])
+print(mean_temperature)
+
+for i in range(1,13):
+    month=data.loc[data["month"]==i]["Temperature(C)"]
+    print("For month: "+str(i))
+    print("Average Temperature is: "+str(np.mean(month)))
+    print("The temperature std is: "+str(np.std(month)))
